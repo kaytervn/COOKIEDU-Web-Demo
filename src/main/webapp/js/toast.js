d8252de07d50d553c6d9fee4b4d0b0d56edf3b4d
KayterCantFixBug <118961250+KayterCantFixBug@users.noreply.github.com>
2024-02-18 23:37:22 +0700

    a

--- src/main/webapp/js/toast.js
+++ src/main/webapp/js/toast.js
@@ -2,8 +2,8 @@ const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 
 if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-  })
+	const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+	toastTrigger.addEventListener('click', () => {
+		toastBootstrap.show()
+	})
 }
